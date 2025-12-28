9354adff194612adad4e367d82c86cb4be9f40b0
gopu-ganesh <122140351+MManisha03@users.noreply.github.com>
2025-12-28 12:26:06 -0500

    wsw

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,6 @@ COPY . .
 RUN npm run build
 
 # Cloud Run will inject PORT=8080
-EXPOSE 8080
+EXPOSE 5173
 
 CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5173;
 
 // Serve static Vite files
 app.use(express.static(path.join(__dirname, "dist")));
